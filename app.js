abba8f03781cde5ba79e391ded889e0de0bc6121
eyuelt <eyueltessema@gmail.com>
2014-02-06 02:20:51 -0800

    Change port to 8000

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var index = require('./routes/index');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
