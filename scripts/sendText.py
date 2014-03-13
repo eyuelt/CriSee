b4b144afacaca2e6a5ac3cd858662ec4de726afb
eyuelt <eyueltessema@gmail.com>
2014-03-13 00:02:28 -0700

    Modify sendText.py and sms.py to not use pexpect, which doesn't work on heroku

--- scripts/sendText.py
+++ scripts/sendText.py
@@ -1,25 +1,19 @@
-#Add the pexpect directory to our PYTHONPATH so we
-#can use the module without installing it
+#Add the pygooglevoice directory to our PYTHONPATH so we
+#can use the googlevoice module without installing it
 import sys
-sys.path.append("../python_modules/pexpect-master")
+sys.path.append("../python_modules/pygooglevoice-0.5")
 
-import pexpect
-import os
+from googlevoice import Voice
+from googlevoice.util import input
 
 def main(number, message):
-  currfilearr = os.path.abspath(__file__).split("/");
-  currfilearr[-1] = "sms.py";
-  scriptfilename = "/".join(currfilearr);
+  voice = Voice();
 
-  child = pexpect.spawn("python " + scriptfilename);
-  child.expect("Email address:");
-  child.sendline("criseeapp@gmail.com");
-  child.expect("Password:");
-  child.sendline("cs147crisee");
-  child.expect("Number to send message to:");
-  child.sendline(number);
-  child.expect("Message to send:");
-  child.sendline(message);
+  email = 'criseeapp@gmail.com';
+  password = 'cs147crisee';
+  voice.login(email, password);
+
+  voice.send_sms(number, message);
 
 
 if __name__ == "__main__":
