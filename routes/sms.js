80c1874bcee4f9c209f82d07047b0c488968ad43
eyuelt <eyueltessema@gmail.com>
2014-03-13 18:00:22 -0700

    Decompose sendText method

--- routes/sms.js
+++ routes/sms.js
@@ -4,14 +4,17 @@ exports.sendText = function(req, res) {
     var number = req.query.number;
     var message = req.query.message;
 
-    var currdirarr = __dirname.split("/");
-    currdirarr[currdirarr.length-1] = "scripts";
-    currdirarr.push("sendText.py");
-    var scriptfile = currdirarr.join("/");
-
-    var command = 'python ' + scriptfile + ' ' + number + ' "' + message + '"';
-    console.log('Sending text using command: [' + command + ']');
-    exec(command);
-
+    sendTextMessage(number, message);
     res.send(200);
 };
+
+function sendTextMessage(number, message) {
+  var currdirarr = __dirname.split("/");
+  currdirarr[currdirarr.length-1] = "scripts";
+  currdirarr.push("sendText.py");
+  var scriptfile = currdirarr.join("/");
+
+  var command = 'python ' + scriptfile + ' ' + number + ' "' + message + '"';
+  console.log('Sending text using command: [' + command + ']');
+  exec(command);
+ };
